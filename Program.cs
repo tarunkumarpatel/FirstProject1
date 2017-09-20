a4fef4428551abf148fa1424ac4c0fe6bbcaeac9
Tarun Patel <tarunkumar.pateli@einfochips.com>
2017-09-20 16:59:35 +0530

    cmt

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DesignPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Singleton Emp= Singleton.GetInstance;
+            //Emp.Print("hii All Employyee");
+            //Emp.Print("how r u all ?");
+
+            //Singleton std = Singleton.GetInstance;
+            //std.Print("hii All Student");
+            //std.Print("how r u all ?");
+            try
+            {
+                string value = null;
+                if (value.Length == 0) // <-- Causes exception
+                {
+                    Console.WriteLine(value); // <-- Never reached
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                Console.ReadKey();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Singleton.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DesignPattern
+{
+    public sealed class Singleton
+    {
+        private static int _counter = 0;
+        private static Singleton _instance = null;
+        public static Singleton GetInstance
+        {
+            get
+            {
+                if (_instance != null) return _instance;
+                _instance=new Singleton();
+                return _instance;
+                
+            }
+        }
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Singleton"/> class.
+        /// </summary>
+        private Singleton()
+        {
+            _counter++;
+            Console.WriteLine("counter:- {0}",_counter);
+        }
+
+      
+
+        public void Print(string msg)
+        {
+            Console.WriteLine(msg);
+        }
+    }
+}
